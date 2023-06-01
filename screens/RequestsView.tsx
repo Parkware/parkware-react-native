13551ea73519595ddf68446d794fbf7ce167ce67
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-06-01 13:41:29 -0400

    working request view...yay!

--- screens/RequestsView.tsx
+++ screens/RequestsView.tsx
@@ -68,7 +68,7 @@ export function RequestsView() {
       </Text>
       
       {eventData.map((event) => (
-        <View style={{ marginBottom: 10 }}>
+        <View style={{ marginBottom: 10 }} key={event.id}>
         <Text key={event.doc.endTime}>
           {'Name: ' + event.doc.name}
         </Text>

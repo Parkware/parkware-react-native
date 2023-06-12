c331fe071a0cfc2b5be58775d91128d3bebd7036
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-06-12 17:20:11 -0400

    not working w/ local deployment...firebase undef

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,15 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const {functions} = require("firebase-functions")
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Wassup World!");
-});
+export const onMessageUpdate = functions.firestore.document("/events/{docID}")
+.onWrite(async (change: any, context: any) => {
+    const after = change.after.data();
+    console.log(after);
+    return getFirestore().doc(`/users/${after}`).set({ dummy_message: "You were accepted for event {docID}" })
+})

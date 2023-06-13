1160ae9465ebf2b11204a6a4080b303534fa0953
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-06-13 08:35:58 -0400

    working firestore cf code-->not build:watch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,17 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const {functions} = require("firebase-functions")
+const functions = require("firebase-functions")
 
 // The Firebase Admin SDK to access Firestore.
 const {initializeApp} = require("firebase-admin/app");
 const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
+const db = getFirestore();
 
 export const onMessageUpdate = functions.firestore.document("/events/{docID}")
 .onWrite(async (change: any, context: any) => {
     const after = change.after.data();
-    console.log(after);
-    return getFirestore().doc(`/users/${after}`).set({ dummy_message: "You were accepted for event {docID}" })
+    const d = after.accepted_id;
+    console.log(d);
+    return db.doc(`/users/${d}`).set({ dummy_message: "You were accepted for event {docID}" });
 })

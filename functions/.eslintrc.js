6b812b725d6066867983dd9e822cb1867d00fe4f
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-06-15 20:14:12 -0400

    added require back

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "@typescript-eslint/no-var-requires": 0,
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-import functions from "firebase-functions";
+const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
 const db = getFirestore();
@@ -12,6 +12,6 @@ export const onMessageUpdate = functions.firestore.document("/events/{docID}")
   .onWrite(async (change: any, context: any) => {
     const after = change.after.data();
     const d = after.accepted_id;
-    const event_id = context.params.docID;
-    return db.doc(`/users/${d}`).set({ accepted_events: event_id });
-})
+    const eventId = context.params.docID;
+    return db.doc(`/users/${d}`).set({accepted_events: eventId});
+  });

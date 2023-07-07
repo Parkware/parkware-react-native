da805df2b19a4c9f7d9ad2990143bdc596459294
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-07-06 22:09:40 -0400

    removed extra code

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import messaging from "@react-native-firebase/messaging"
 
 // Initialize Firebase
 const firebaseConfig = {
--- screens/providerComponents/ProviderRequestsView.tsx
+++ screens/providerComponents/ProviderRequestsView.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, SafeAreaView, Button, TouchableOpacity } from 'react-native';
-import { DocumentData, arrayUnion, collection, doc, getDoc, onSnapshot, setDoc, updateDoc } from 'firebase/firestore';
+import { DocumentData, arrayUnion, collection, doc, getDoc, getDocs, onSnapshot, query, setDoc, updateDoc, where } from 'firebase/firestore';
 import { auth, db } from '../../firebaseConfig';
 import 'firebase/firestore';
 import { signOut } from 'firebase/auth';
@@ -33,6 +33,7 @@ export function ProviderRequestsView() {
   const [accEvents, setAccEvents] = useState<docDataPair[]>([]);
   const [pendingEvents, setPendingEvents] = useState<docDataPair[]>([]);
   const [unwantedEvents, setUnwantedEvents] = useState<string[]>([]);
+  const [deniedEventArr, setDeniedEventArr] = useState([]);
 
   const navigation = useNavigation<providerScreenProp>();
 
@@ -44,6 +45,12 @@ export function ProviderRequestsView() {
     }
   };
   
+  const updateDeniedEvents = async () => {
+    const q = query(collection(db, 'events'), where('unwantedProviders', 'array-contains', auth.currentUser!.uid))
+    const eventsSnap = await getDocs(q);
+    eventsSnap.docs.map((e: DocumentData) => e.doc.eventName)
+  };
+
   // Reading the event data and setting eventData to it. 
   useEffect(() => {
     try {
@@ -92,33 +99,18 @@ export function ProviderRequestsView() {
   }
   
   const updateDB = async (eventData: docDataPair) => {
-    const curEventRef = doc(db, 'events/', eventData.id);
-    const currUid = auth.currentUser!.uid;
-    if (eventData.doc.consumer_id != currUid) {
-      const currUserRef = doc(db, 'users/', currUid);
-      const currUserSnap = await getDoc(currUserRef);
-      if (currUserSnap.exists()) {
-        let already_providing = false;
-        const eventSnap = await getDoc(curEventRef);
-        // Eventually, this won't be needed since we would disable if already sent, but just to validate
-        if (eventSnap.exists()) {
-          eventSnap.data().interestedProviders.forEach((prov: any | undefined) => {
-            if (prov.id == currUid) already_providing = true
-          });
-        }
-        if (!already_providing) {
-          await setDoc(curEventRef, {
-            interestedProviders: arrayUnion({
-              id: currUid,
-              name: currUserSnap.data().name,
-              address: currUserSnap.data().address,
-              providerSpaces: currUserSnap.data().providerSpaces
-            }),
-            interestedProviderIds: arrayUnion(currUid),
-          }, { merge: true });
-        }
-      }
-    }
+    const id = auth.currentUser!.uid;
+    const currUserSnap = await getDoc(doc(db, 'users/', id));
+    if (currUserSnap.exists())
+      await setDoc(doc(db, 'events/', eventData.id), {
+        interestedProviders: arrayUnion({
+          id,
+          name: currUserSnap.data().name,
+          address: currUserSnap.data().address,
+          providerSpaces: currUserSnap.data().providerSpaces
+        }),
+        interestedProviderIds: arrayUnion(id),
+      }, { merge: true });
     /*
     A provider may need to be able to revert their acceptance to an event request. 
     else {
@@ -133,7 +125,6 @@ export function ProviderRequestsView() {
       <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
         Accepted Requests
       </Text>
-      
       {accEvents.map(event => (
         <TouchableOpacity style={{ marginBottom: 10 }} key={event.id} onPress={() => navigation.navigate('consumerStatusView', { event })}>
           <Text style={{ fontSize: 15 }}>Click here to see more info about your provider</Text>
@@ -145,7 +136,6 @@ export function ProviderRequestsView() {
       <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
         Pending Requests
       </Text>
-      
       {pendingEvents.map((event) => (
         <View style={{ marginBottom: 10 }} key={event.id}>
           <EventBlock event={event} showSpaces={true}/>
@@ -155,9 +145,8 @@ export function ProviderRequestsView() {
       <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
         Open Requests
       </Text>
-      
       {openEvents
-        .filter((e: DocumentData) => !unwantedEvents.includes(e.id))
+        .filter((e: DocumentData) => !unwantedEvents.includes(e.id) && e.doc.consumer_id !== auth.currentUser!.uid)
         .map((event) => (
         <View style={{ marginBottom: 10 }} key={event.id}>
           <EventBlock event={event} showSpaces={true}/>
@@ -165,6 +154,9 @@ export function ProviderRequestsView() {
           <Button title='Decline' onPress={() => removeLocalEventData(event.id)}/>
         </View>
       ))}
+      <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
+        Denied Events
+      </Text>
       <Button title="Log out" onPress={logout} />
     </SafeAreaView>
     </View>

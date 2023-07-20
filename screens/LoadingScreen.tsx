a19397d291a551d4fbf1df31fb80ee7262b1a2bd
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-07-20 17:58:27 -0400

    finally! app working! yay

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import { Text, View } from 'react-native'
+import React from 'react'
 
 const LoadingScreen = () => {
   return (
